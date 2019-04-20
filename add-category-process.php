88ad80533ba67d8a395f76d0fce2236f748b0071
Michael <mikelignacio@gmail.com>
2019-04-20 18:49:19 +0800

    Fixed indentation

--- add-category-process.php
+++ add-category-process.php
@@ -1,25 +1,24 @@
 <?php
-$connect=mysqli_connect('localhost','root','','safemedpharmacy');
+$connect = mysqli_connect('localhost','root','','safemedpharmacy');
 
-			$name = mysqli_real_escape_string($connect,$_POST['name']);
-			$login_type = mysqli_real_escape_string($connect,$_POST['login_type']);
-			$category_name = mysqli_real_escape_string($connect,$_POST['category_name']);
-			
-			date_default_timezone_set('Asia/Manila');
-			$date_today = date('F j, Y');
+$name = mysqli_real_escape_string($connect,$_POST['name']);
+$login_type = mysqli_real_escape_string($connect,$_POST['login_type']);
+$category_name = mysqli_real_escape_string($connect,$_POST['category_name']);
 
-			$query = "INSERT INTO dim_category(category_name, added_by, date_added) 
-			VALUES ('$category_name', '$name', '$date_today')";
+date_default_timezone_set('Asia/Manila');
+$date_today = date('F j, Y');
 
-			if(mysqli_query($connect, $query)){
-			header( "Location: categories.php" ); die;
-			echo "<script>window.open('categories.php','_self')</script>";
-			}
-			if(mysqli_connect_errno($connect))
-			{
-				echo 'Failed to connect';
-			}
-		
-			
+$query = "INSERT INTO dim_category(category_name, added_by, date_added) 
+VALUES ('$category_name', '$name', '$date_today')";
+
+if(mysqli_query($connect, $query)){
+	header( "Location: categories.php" ); die;
+	echo "<script>window.open('categories.php','_self')</script>";
+}
+if(mysqli_connect_errno($connect)) {
+	echo 'Failed to connect';
+}
+
+
 ?>
 
