4cc95183c5cc07778405b951c298ab7634bac4cc
Michael <mikelignacio@gmail.com>
2019-04-21 09:35:25 +0800

    Updated all pages to include meta.php

--- log-management.php
+++ log-management.php
@@ -21,11 +21,7 @@
 
 <head>
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+  <?php include('meta.php'); ?>
 
   <title>SafeMed Pharmacy - Log Management</title>
 
--- view-stock-card.php
+++ view-stock-card.php
@@ -5,11 +5,7 @@
 
 <head>
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+  <?php include('meta.php'); ?>
 
   <title>SafeMed Pharmacy - View Stock Card by SKU</title>
 
