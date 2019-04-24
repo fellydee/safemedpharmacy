8db6a7657a0c22f04ff2e39b65e748a9172a09c0
Lyca Ignacio <lycaignacio@gmail.com>
2019-04-24 23:14:32 +0800

    Fixed cancel purchase order function

--- delete-purchase-order-process.php
+++ delete-purchase-order-process.php
@@ -1,14 +1,22 @@
 <?php
+
+// maglagay ka ng session start dito
+ session_start();
+
 $connect=mysqli_connect('localhost','root','','safemedpharmacy');
 
 $ref_num = $_POST['ref_num'];
+$_SESSION['ref_num'] = $ref_num;
+
+$_SESSION['is_cancelled'] = FALSE;
 
 $query = "DELETE FROM dim_inventory WHERE ref_num = $ref_num";
 
-if(mysqli_query($connect, $query)){
-	$item_id = mysqli_insert_id($connect);
-	$ref_num += $item_id;
-	header( "Location: purchase-order.php" ); die;
+if (mysqli_query($connect, $query)) {
+	$_SESSION['is_cancelled'] = TRUE;
+
+	header( "Location: purchase-order.php" ); 
+	die;
 	echo "<script>window.open('purchase-order.php','_self')</script>";
 } elseif(mysqli_connect_errno($connect)) {
 	echo 'Failed to connect';
