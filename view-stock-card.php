909be1b5bf16c6212a0dd4769bdf1d917de27e27
Lyca Ignacio <lycaignacio@gmail.com>
2019-04-25 22:21:39 +0800

    Decreased the font size of Stock Card title in view-stock-card.php

--- view-stock-card.php
+++ view-stock-card.php
@@ -43,7 +43,7 @@
 
           <!-- Page Heading -->
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Stock Card for <?php echo $medicine_name; ?></h1>
+            <h1 class="h4 mb-0 text-gray-800">Stock Card for <?php echo $medicine_name; ?></h1>
           </div>
 
         <?php if (isset($_SESSION['productFound']) && $_SESSION['productFound'] === false): ?>
