525af80c72a135d87b2e2d359c7310013755c8fb
Michael <mikelignacio@gmail.com>
2019-04-30 11:29:01 +0800

    Updated login page

--- login.php
+++ login.php
@@ -15,7 +15,6 @@
 <body class="bg-gradient-primary login">
 
   <div class="container">
-
     <!-- Outer Row -->
     <div class="row justify-content-center">
 
@@ -25,11 +24,9 @@
           <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
-              <div class="col-lg-12">
-                <div class="p-5">
-                  <div>
-                    <h1 class="h4 text-gray-900 mb-4"><b>Login to SafeMed Pharmacy</b></h1>
-                  </div>
+              <div class="col-lg-12 main-column">
+                <img src="logo_transparent.png" alt="" class="aligncenter login-logo" width="400">
+                <div class="p-5 login-form">
                   <form class="user" action="login-process.php" method="post">
                     <div class="form-group">
                       <input type="text" class="form-control form-control-user" name="username" placeholder="Username">
@@ -45,13 +42,6 @@
                     </div>
                     <button type="submit" class="btn btn-primary btn-user btn-block">Login</button>
                   </form>
-                  <hr>
-                  <div class="text-center">
-                    <a class="small" href="forgot-password.html">Forgot Password?</a>
-                  </div>
-                  <div class="text-center">
-                    <a class="small" href="register.html">Create an Account!</a>
-                  </div>
                 </div>
               </div>
             </div>
