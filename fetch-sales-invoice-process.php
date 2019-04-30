00f6ce57a8dc1c3376fbe126a6475cf843a08f5c
Michael <mikelignacio@gmail.com>
2019-04-30 10:17:04 +0800

    Removed unnecessary code

--- fetch-sales-invoice-process.php
+++ fetch-sales-invoice-process.php
@@ -1,17 +1,7 @@
 <?php
 $connect = mysqli_connect('localhost','root','','safemedpharmacy');
 
-if(!$_SESSION['username']) {
-    header("Location: login.php");
-}
-else {
-    $username = $_SESSION['username'];
-    $name = $_SESSION['name'];
-    $login_type = $_SESSION['login_type'];
-}
-
 $salesData = array();
-$_SESSION['sku'] = $_POST['sku'];
 $is_sold = false;
 
 $query = "SELECT date_added, item_id, quantity FROM dim_orders WHERE sku=" . mysqli_escape_string($connect, $_POST['sku']);
@@ -19,8 +9,10 @@ $query = "SELECT date_added, item_id, quantity FROM dim_orders WHERE sku=" . mys
 $result_set = mysqli_query($connect, $query);
 
 if (mysqli_num_rows($result_set) == 0) {
-	header('Location: stock-card.php');
-	die();
+	// header('Location: stock-card.php');
+	// die();
+
+	// no products sold
 } else {
 	$is_sold = true;
 	while($row = mysqli_fetch_assoc($result_set)) {
