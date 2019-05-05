f550f032e9e2c221df5b3bd774c63d4948c979e1
Lyca Ignacio <lycaignacio@gmail.com>
2019-05-05 23:15:10 +0800

    Changed Earnings to Profit in dashboard

--- index.php
+++ index.php
@@ -121,13 +121,13 @@ if(!$_SESSION['username']) {
 					<!-- Content Row -->
 					<div class="row financial-earnings">
 
-						<!-- Earnings (Monthly) Card Example -->
+						<!-- Profit (Monthly) Card Example -->
 						<div class="col-xl-3 col-md-6 mb-4">
 							<div class="card border-left-primary shadow h-100 py-2">
 								<div class="card-body">
 									<div class="row no-gutters align-items-center">
 										<div class="col mr-2">
-											<div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Earnings (Monthly)</div>
+											<div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Profit (Monthly)</div>
 											<div class="h5 mb-0 font-weight-bold text-gray-800">
 												<?php 
 												$count = mysqli_query($connect, "SELECT SUM(price) from dim_orders where MONTH(date_added) = MONTH(CURRENT_DATE()) AND YEAR(date_added) = YEAR(CURDATE())");
@@ -146,13 +146,13 @@ if(!$_SESSION['username']) {
 							</div>
 						</div>
 
-						<!-- Earnings (Monthly) Card Example -->
+						<!-- Profit (Monthly) Card Example -->
 						<div class="col-xl-3 col-md-6 mb-4">
 							<div class="card border-left-success shadow h-100 py-2">
 								<div class="card-body">
 									<div class="row no-gutters align-items-center">
 										<div class="col mr-2">
-											<div class="text-xs font-weight-bold text-success text-uppercase mb-1">Earnings (Annual)</div>
+											<div class="text-xs font-weight-bold text-success text-uppercase mb-1">Profit (Annual)</div>
 											<div class="h5 mb-0 font-weight-bold text-gray-800">
 												<?php 
 												$count = mysqli_query($connect, "SELECT SUM(price) from dim_orders where YEAR(date_added) = YEAR(CURDATE())
@@ -172,7 +172,7 @@ if(!$_SESSION['username']) {
 							</div>
 						</div>
 
-						<!-- Earnings (Monthly) Card Example -->
+						<!-- Profit (Monthly) Card Example -->
 						<div class="col-xl-3 col-md-6 mb-4">
 							<div class="card border-left-info shadow h-100 py-2">
 								<div class="card-body">
